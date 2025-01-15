0cbb3c1e9a7c80ffae945b2542c688b2bd066ba9
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-15 18:45:48 +0200

    Create Question Class

---
+++ Exam02/Question/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam02.Question
+{
+    internal abstract class Question
+    {
+        public required string Header { get; set; }
+        public required string Body { get; set; }
+        public int Mark {  get; set; }
+        public Question(string Header ,string Body ,int Mark)
+        {
+            this.Header = Header;
+            this.Body = Body;
+            this.Mark = Mark;
+        }
+
+
+    }
+}
